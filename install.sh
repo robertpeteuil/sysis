d8a92f40de40c2c427bfbbec0aff2f3dd557d64b
Robert Peteuil <robert@peteuil.com>
2017-03-24 17:53:51 -0700

    Refine Installer by Removing Debug Code and Comments

--- install.sh
+++ install.sh
@@ -2,16 +2,12 @@
 
 REPOACCT="robertpeteuil"
 REPONAME="sysis"
-# DEBUG=true
 
 CURRENTDIR="$(pwd)"
-CURRENTDIR=${CURRENTDIR##*\/}    # remove leading dirs
-
-# UTILTMPDIR="${REPOACCT}-${REPONAME}*"
+CURRENTDIR=${CURRENTDIR##*\/}
 UTILTMPDIR="${REPOACCT}-${REPONAME}"
 
 cleanupDownload() {
-  [[ "$DEBUG" ]] && echo -e "\tcleaning up"
   cd "${TMPDIR}" || exit 1
   rm -rf "${UTILTMPDIR}"
 }
@@ -21,76 +17,51 @@ cleanupDownload() {
 #   current dir-name = package DL name (from extracting tar or zip)
 if [[ ! ("${CURRENTDIR}" =~ "${UTILTMPDIR}"*) && ! (("${CURRENTDIR}" =~ "${REPONAME}"*)) ]]; then
 
-  [[ "$DEBUG" ]] && echo "not in repo dir"
-  # not in repo directory (it needs to be DL'd)
-
-  # switch to temp dir
-  TMPDIR=${TMPDIR:-/tmp}
-  [[ "$DEBUG" ]] && echo -e "\tswitching to temp dir: ${TMPDIR}"
-  cd "${TMPDIR}" || exit 1
-
   # find latest repo package, download and un-tar it
+  TMPDIR=${TMPDIR:-/tmp}
+  cd "${TMPDIR}" || exit 1
   PKGDLURL=$(curl -s https://api.github.com/repos/${REPOACCT}/${REPONAME}/releases/latest | grep tarball_url | head -n 1 | cut -d '"' -f 4)
-  [[ "$DEBUG" ]] && echo -e "\tdownloading repo: ${PKGDLURL}"
   curl -sL "$PKGDLURL" | tar zxf -
 
-  [[ "$DEBUG" ]] && echo -e "\tcalculating extraction dir name"
   # find the name of the dir extracted to, as it has the last commit # as a suffix
   UTILTMPDIR=$(ls -d -t ${REPOACCT}-${REPONAME}* | head -n 1)
   UTILTMPDIR=${UTILTMPDIR/%\//}
-
-  [[ "$DEBUG" ]] && echo -e "\tchanging to extraction dir: ${UTILTMPDIR}"
-  # cd repo dir
   cd "${UTILTMPDIR}" || exit 1
-
   CLEANUPREQ=true
-
-else
-
-  [[ "$DEBUG" ]] && echo "we're in a dir named for the repo"
-  # assume the repo is downloaded and were doing a local install
-
 fi
 
-# install code
-[[ "$DEBUG" ]] && echo -e "Installer"
+# determine destination
 if [[ -w "/usr/local/bin" ]]; then
-  [[ "$DEBUG" ]] && echo -e "\t/usr/local/bin writeable - installing there"
   BINDIR="/usr/local/bin"
   CMDPREFIX=""
   STREAMLINED=true
 else
-  [[ "$DEBUG" ]] && echo -e "\t/usr/local/bin not writeable by current user\n\n"
   echo -e "installer - ${REPONAME} utility\n"
   echo "Specify install directory (a,b or c):"
   echo -en "\t(a) '~/bin'    (b) '/usr/local/bin' as root    (c) abort : "
   read -r -n 1 SELECTION
   echo
-
   if [ "${SELECTION}" == "a" ] || [ "${SELECTION}" == "A" ]; then
-    [[ "$DEBUG" ]] && echo "Option A selected: installing into ${HOME}/bin"
     BINDIR="${HOME}/bin"
     CMDPREFIX=""
   elif [ "${SELECTION}" == "b" ] || [ "${SELECTION}" == "B" ]; then
-    [[ "$DEBUG" ]] && echo "Option B selected: installing into '/usr/local/bin' as root"
     BINDIR="/usr/local/bin"
     CMDPREFIX="sudo "
   else
-    [[ "$DEBUG" ]] && echo "Aborting"
     [[ "${CLEANUPREQ}" ]] && cleanupDownload
     exit 0
   fi
-
 fi
 
-# Install
+# install
 mkdir -p "${BINDIR}"
 ${CMDPREFIX} cp -f sysis "${BINDIR}"
 
-# cleanup
 if [[ "${CLEANUPREQ}" ]]; then
   cleanupDownload
 fi
 
 [[ ! "$STREAMLINED" ]] && echo
 echo "sysis utility installed to ${BINDIR}"
+
+exit 0
